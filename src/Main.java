00fdfb9ec718daf82b483e362fae9fe21fe24253
Jakob Engwall <jakob.engwall@gmail.com>
2023-10-26 11:12:48 +0200

    Test Commit 1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Branch 1");
+        System.out.println("mergea det här med main");
 
         }
     }
